8e980143443298293685b5988e20bd1588155752
chaechaeyeon <00dreamadream@kakao.com>
2024-07-28 17:41:16 +0900

    ADD: 여행지 별 리뷰 조회

--- src/main/java/seomile/api/travel/contoller/TravelController.java
+++ src/main/java/seomile/api/travel/contoller/TravelController.java
@@ -1,13 +1,14 @@
 package seomile.api.travel.contoller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import seomile.api.review.dto.ReviewDTO;
+import seomile.api.review.service.ReviewService;
 import seomile.api.travel.dto.TravelDTO;
 import seomile.api.travel.dto.TravelListDTO;
 import seomile.api.travel.service.TravelService;
+
 
 import java.util.List;
 
@@ -17,6 +18,8 @@ public class TravelController {
 
     @Autowired
     private TravelService travelService;
+    @Autowired
+    private ReviewService reviewService;
 
     @GetMapping("/list")
     public List<TravelListDTO> getTravelList(@RequestParam(required = false) List<Integer> categories) {
@@ -28,6 +31,12 @@ public class TravelController {
         return travelService.fetchTravelDetailInfo(travelCode);
     }
 
+    //관광지별 해당 리뷰 조회
+    @GetMapping("/{travCode}/review/{reviewId}")
+    public ResponseEntity<ReviewDTO> getReviewByTravelCodeAndReviewId(@PathVariable("travCode") String travCode, @PathVariable("reviewId") Long reviewId) {
+        ReviewDTO review = reviewService.getReviewByTravelCodeAndReviewId(travCode, reviewId);
+        return ResponseEntity.ok(review);
+    }
 
 //    @GetMapping("/조회좀하자")
 //    public TravelDTO getTravelList(String travelCode) {
